deb572d2d18f3c17831729dfdf6ae394075cb86c
TaeMochi12 <himanshi4821@gmail.com>
2023-07-16 15:12:27 +0530

    Dictionary methods added

---
+++ dictmethods.py
@@ -0,0 +1,20 @@
+ep1 = {122: 45, 143: 34, 321: 42, 756: 26}
+
+ep2 = {322: 86, 128: 44, 908: 84}
+
+# ep1.update(ep2)  #updates  the values of ep1 with ep2 which combines the ep2 values in ep1
+
+# ep1.clear()  #to clear the dictionary items
+
+# empty = {}   #to make an empty dictionary
+# print(empty)
+
+# ep1.pop(143)  #removes the corresponding key value pair
+
+# ep1.popitem()  # remvoes the last key value pair
+
+# del ep1  #deletes the dictionary
+
+# del ep1[143]  # deletes the particular entry
+
+print(ep1)
